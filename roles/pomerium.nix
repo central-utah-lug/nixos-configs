75e3edef042ae6a711fad56087525e9e3ee84783
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-05-17 19:02:21 -0600

    added envoy to permittedInsecurePackages for now
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- roles/pomerium.nix
+++ roles/pomerium.nix
@@ -6,6 +6,10 @@
     443
   ];
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "envoy-1.25.1"
+  ];
+
   services.pomerium = {
     enable = true;
     configFile = /opt/pomerium/config.yaml;
